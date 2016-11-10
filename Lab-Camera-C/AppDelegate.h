9ee61eff502d667b58f9bcc897d2f7cc08904d12
Rui <rui@Students-MacBook-Pro.local>
2016-11-10 11:52:42 -0500

    Initial Commit

---
+++ Lab-Camera-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab-Camera-C
+//
+//  Created by Rui on 2016-11-10.
+//  Copyright © 2016 Rui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lab-Camera-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab-Camera-C
+//
+//  Created by Rui on 2016-11-10.
+//  Copyright © 2016 Rui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
